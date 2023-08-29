03c6a20d73afaf093a9684826742318f34aa1663
Ahmed-Ouakad <ahmedouakad@gmail.com>
2023-08-29 13:36:48 +0100

    all changes

--- expense_tracker/lib/presentation/screens/home_screen.dart
+++ expense_tracker/lib/presentation/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../theme/app_colors.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
   @override
@@ -9,9 +11,33 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Text("Home Screen")
+    return DefaultTabController(
+      
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: splashScreenLogoColor,
+          title: const Text("ExpenseTracker",style: TextStyle(
+              fontFamily: 'Lobster',
+              fontSize: 25
+          ),
+          ),
+          bottom: TabBar(
+            automaticIndicatorColorAdjustment: false,
+            tabs: [Tab(text: "home",),Tab(text: "Profile",)],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(
+              child:  Text("Home Screen"),
+            ),
+            Center(
+              child:  Text("Profile Screen"),
+            ),
+          ],
+        )
       ),
     );
   }
